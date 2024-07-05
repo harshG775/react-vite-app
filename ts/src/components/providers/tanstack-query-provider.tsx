8297b1a5a1e8da3dc878a3477107bcfce4cc7b4b
Harsh <hgaur491@gmail.com>
2024-07-05 14:16:42 +0530

    installing tanstack query

--- ts/src/components/providers/tanstack-query-provider.tsx
+++ ts/src/components/providers/tanstack-query-provider.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 const queryClient = new QueryClient();
 
-type TQueryProps = PropsWithChildren<{}>;
+type TQueryProps = PropsWithChildren
 export default function TanstackQueryProvider({ children }: TQueryProps) {
     return (
         <QueryClientProvider client={queryClient}>
