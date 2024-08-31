2289ef7db8664f954dac712d5adf58a460740cfc
Harsh <hgaur491@gmail.com>
2024-08-31 20:21:03 +0530

    installing shadcn  in vite-ts & deleting old ts

--- vite-ts/src/pages/Page.tsx
+++ vite-ts/src/pages/Page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
     return (
-        <main className="grid place-content-center min-h-screen">
+        <main className="grid place-content-center min-h-screen gap-y-4">
             <div className="text-3xl font-bold underline">Root page</div>
+            <Button>Click me</Button>
         </main>
     );
 }
